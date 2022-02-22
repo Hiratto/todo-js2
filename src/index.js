cf5733fc92a9685e45d40115e5e42c1924257565
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-02-22 21:53:21 +0900

    共通化（未完了リストから指定の要素を削除）

--- src/index.js
+++ src/index.js
@@ -19,8 +19,7 @@ const onClickAdd = () => {
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
     // 押された完了ボタンの親タグ（div)を未完了リストから削除
-    const deleteTaret = completeButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTaret);
+    deleteFromIncompleteList(completeButton.parentNode);
   });
 
   // button（削除）タグ生成
@@ -28,8 +27,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     // 押された削除ボタンの親タグ（div)を未完了リストから削除
-    const deleteTaret = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTaret);
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を設定
@@ -39,6 +37,11 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
+
+  // 未完了リストから指定の要素を削除
+  const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 };
 
 document
